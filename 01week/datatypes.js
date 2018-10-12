26219bfb7553b11bd8d27914317830630bd78cb9
DMuttz <dmutter1987@gmail.com>
2018-10-11 20:01:02 -0500

    submitting half done data types assignmentI need to complete this

---
+++ 01week/datatypes.js
@@ -0,0 +1,27 @@
+// 1. Write a JavaScript program to display the current day and time.
+const todaysDate = new Date();
+console.log(todaysDate);
+
+// 2. Write a JavaScript program to convert a number to a string.
+const num = 50;
+const n = num.toString();
+n;
+
+// 3. Write a JavaScript program to convert a string to the number.
+Number('100');
+
+// 4. Write a JavaScript program that takes in different datatypes and prints out whether they are a...
+console.log (typeof true);
+console.log (typeof null);
+console.log (typeof -1000);
+console.log (typeof 200);
+console.log (typeof IEEE754);
+console.log (typeof "words");
+
+// 5. Write a JavaScript program that adds 2 numbers together.
+const add =(num1, num2)=> {
+  return num1 + num2
+}
+add(6,8)
+
+// 6. Write a JavaScript program that runs only when 2 things are true.
