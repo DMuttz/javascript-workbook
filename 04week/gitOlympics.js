897118d69dc5aed3cc21d5c4f8a6a2529f6f379f
DMuttz <dmutter1987@gmail.com>
2018-10-25 19:04:53 -0500

    added function

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const printListOfOlympians = (arr) =>{
+ arr.forEach(element => {
+     console.log(element)
+ })
+}
+
+printListOfOlympians(["daniel", "jim", "bob", "allen", "jim"]);
