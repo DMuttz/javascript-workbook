270c3bd4d28f16eb7bed4304426e0e8c385ef8e4
DMuttz <dmutter1987@gmail.com>
2018-11-06 16:55:54 -0600

    removed datatypes

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -1,6 +1,17 @@
 'use strict';
 
-
+// User1 input of rock, paper, or scissors.
+// User2 input of rock, paper, or scissors.
+// Compare User1 input to User2 input.
+// If User1 input is 'rock' and User2 input is 'scissor', User1 wins.
+// If User1 input is 'rock' and User2 input is 'paper', User2 wins.
+// If User1 input is 'rock' and User2 input is 'rock', it's a tie.
+// If User1 input is 'paper' and User2 input is 'rock', User1 wins.
+// If User1 input is 'paper' and User2 input is 'scissors', User2 wins.
+// If User1 input is 'paper' and User2 input is 'paper', it's a tie.
+// If User1 input is 'scissors' and User2 input is 'paper', User1 wins.
+// If User1 input is 'scissors' and User2 input is 'rock', User2 wins.
+// If User1 input is 'scissors' and User2 input is 'scissors', it's a tie.
 
 const assert = require('assert');
 const readline = require('readline');
@@ -12,7 +23,15 @@ const rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
 
-  // Write code here
+
+// Now that I have a better understanding of how functions work and the idea of returning as truthy or falsy, I realized I can just test for hand 1 wins, and if it isn't those cases or a tie, than hand 2 by default wins reducing many lines of code to just a few!
+if (hand1 === hand2) {
+  return "it's a tie!"
+} else if ((hand1 === "rock" && hand2 === "scissors") || (hand1 === "paper" && hand2 === "rock") || (hand1 === "scissors" && hand2 === "paper")) {
+  return "User 1 wins!"
+} else {
+  return "User 2 wins!"
+}
 
 }
 
